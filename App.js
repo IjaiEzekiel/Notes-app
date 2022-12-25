13635bdf2c95201ae6549e5035f1f31d34197b78
Ijai <ijaiwaba@gmail.com>
2022-12-25 17:54:02 +0100

    A html markdown

---
+++ App.js
@@ -0,0 +1,120 @@
+import React from "react"
+import Sidebar from "./Components/Sidebar"
+import Editor from "./Components/Editor"
+import { data } from "./data"
+import Split from "react-split"
+import {nanoid} from "nanoid"
+import "./styles.css"
+
+
+export default function App() {
+    const [notes, setNotes] = React.useState(
+        () => JSON.parse(localStorage.getItem("notes")) || [])
+    const [currentNoteId, setCurrentNoteId] = React.useState(
+        (notes[0] && notes[0].id) || ""
+    )
+
+
+    React.useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes))
+    }, [notes]) 
+    
+    function createNewNote() {
+        const newNote = {
+            id: nanoid(),
+            body: "# Type your markdown note's title here"
+        }
+        setNotes(prevNotes => [newNote, ...prevNotes])
+        setCurrentNoteId(newNote.id)
+    }
+    
+    //This does not rearrange the notes
+    // function updateNote(text) {
+    //     setNotes(oldNotes => oldNotes.map(oldNote => {
+    //         return oldNote.id === currentNoteId
+    //             ? { ...oldNote, body: text }
+    //             : oldNote
+    //     }))
+    // }
+
+    // function updateNote(text) {
+        //try to rearrange the most recently modified note to the top
+        // setNotes(oldNotes => {
+            //create a new empty array
+            //loop over the original array
+                //if the id matches
+                    //put the updated note at the begining of the new array
+                //else
+                    //push the old note at the end of the new array
+            //return the new array
+        // })
+    // }
+
+    function updateNote(text) {
+        //try to rearrange the most recently modified note to the top
+        setNotes(oldNotes => {
+            const newArray = []
+            for (let i = 0; i < oldNotes.length; i++) {
+                const oldNote = oldNotes[i]
+                if (oldNote.id === currentNoteId){
+                    newArray.unshift({...oldNote, body: text})
+                } else {
+                    newArray.push(oldNote)
+                }
+            }
+            return  newArray
+        })
+    }
+
+
+    function deleteNote(event, noteId) {
+        event.stopPropagation()
+        setNotes(oldNotes => oldNotes.filter((note) => note.id !== noteId))
+    }
+    function findCurrentNote() {
+        return notes.find(note => {
+            return note.id === currentNoteId
+        }) || notes[0]
+    }
+    
+    return (
+        <main>
+        {
+            notes.length > 0 
+            ?
+            <Split 
+                sizes={[30, 70]} 
+                direction="horizontal" 
+                className="split"
+            >
+                <Sidebar
+                    notes={notes}
+                    currentNote={findCurrentNote()}
+                    setCurrentNoteId={setCurrentNoteId}
+                    newNote={createNewNote}
+                    deleteNote={deleteNote}
+                />
+                {
+                    currentNoteId && 
+                    notes.length > 0 &&
+                    <Editor 
+                        currentNote={findCurrentNote()} 
+                        updateNote={updateNote} 
+                    />
+                }
+            </Split>
+            :
+            <div className="no-notes">
+                <h1>You have no notes</h1>
+                <button 
+                    className="first-note" 
+                    onClick={createNewNote}
+                >
+                    Create one now
+                </button>
+            </div>
+            
+        }
+        </main>
+    )
+}
